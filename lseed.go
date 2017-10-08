44a0637be1636fabc2e99d50604229b284151af1
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-10-08 16:10:53 -0700

    main: switch poller goroutine to use lnd as a backend

--- lseed.go
+++ lseed.go
@@ -1,61 +1,133 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"os/user"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	macaroon "gopkg.in/macaroon.v1"
+
 	log "github.com/Sirupsen/logrus"
-	"github.com/cdecker/kugelblitz/lightningrpc"
-	"github.com/cdecker/lseed/seed"
+	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/macaroons"
+	"github.com/roasbeef/lseed/seed"
 )
 
 var (
-	lightningRpc *lightningrpc.LightningRpc
+	listenAddr = flag.String("listen", "0.0.0.0:53", "Listen address for incoming requests.")
 
-	listenAddr    = flag.String("listen", "0.0.0.0:8053", "Listen address for incoming requests.")
-	rootDomain    = flag.String("root-domain", "lseed.bitcoinstats.com", "Root DNS seed domain.")
-	pollInterval  = flag.Int("poll-interval", 10, "Time between polls to lightningd for updates")
-	lightningSock = flag.String("lightning-sock", "$HOME/.lightning/lightning-rpc", "Location of the lightning socket")
-	debug         = flag.Bool("debug", false, "Be very verbose")
-	numResults    = flag.Int("results", 25, "How many results shall we return to a query?")
+	lndNode = flag.String("lnd-node", "localhost:10009", "The host:port of the backing lnd node")
+
+	rootDomain = flag.String("root-domain", "nodes.lightning.directory", "Root DNS seed domain.")
+
+	pollInterval = flag.Int("poll-interval", 10, "Time between polls to lightningd for updates")
+
+	debug = flag.Bool("debug", false, "Be very verbose")
+
+	numResults = flag.Int("results", 25, "How many results shall we return to a query?")
 )
 
-// Expand variables in paths such as $HOME
-func expandVariables() error {
-	user, err := user.Current()
+var (
+	lndHomeDir             = btcutil.AppDataDir("lnd", false)
+	defaultTLSCertFilename = "tls.cert"
+	tlsCertPath            = filepath.Join(lndHomeDir, defaultTLSCertFilename)
+
+	defaultMacaroonFilename = "readonly.macaroon"
+	defaultMacaroonPath     = filepath.Join(lndHomeDir, defaultMacaroonFilename)
+)
+
+// cleanAndExpandPath expands environment variables and leading ~ in the passed
+// path, cleans the result, and returns it.
+// This function is taken from https://github.com/btcsuite/btcd
+func cleanAndExpandPath(path string) string {
+	// Expand initial ~ to OS specific home directory.
+	if strings.HasPrefix(path, "~") {
+		homeDir := filepath.Dir(lndHomeDir)
+		path = strings.Replace(path, "~", homeDir, 1)
+	}
+
+	// NOTE: The os.ExpandEnv doesn't work with Windows-style %VARIABLE%,
+	// but the variables can still be expanded via POSIX-style $VARIABLE.
+	return filepath.Clean(os.ExpandEnv(path))
+}
+
+// initLightningClient attempts to initialize, and connect out to the backing
+// lnd node as specified by the lndNode ccommand line flag.
+func initLightningClient() (lnrpc.LightningClient, error) {
+	// First attempt to establish a connection to lnd's RPC sever.
+	creds, err := credentials.NewClientTLSFromFile(tlsCertPath, "")
 	if err != nil {
 		return err
 	}
-	*lightningSock = strings.Replace(*lightningSock, "$HOME", user.HomeDir, -1)
-	return nil
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+
+	// Load the specified macaroon file.
+	macPath := cleanAndExpandPath(defaultMacaroonPath)
+	macBytes, err := ioutil.ReadFile(macPath)
+	if err != nil {
+		return nil, err
+	}
+	mac := &macaroon.Macaroon{}
+	if err = mac.UnmarshalBinary(macBytes); err != nil {
+		return nil, err
+	}
+
+	// Now we append the macaroon credentials to the dial options.
+	opts = append(
+		opts,
+		grpc.WithPerRPCCredentials(macaroons.NewMacaroonCredential(mac)),
+	)
+
+	conn, err := grpc.Dial(*lndNode, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to dial to lnd's gRPC server: ",
+			err)
+	}
+
+	// If we're able to connect out to the lnd node, then we can start up
+	// our RPC connection properly.
+	lnd := lnrpc.NewLightningClient(conn)
+
+	return lnd, nil
 }
 
-// Regularly polls the lightningd node and updates the local NetworkView.
-func poller(lrpc *lightningrpc.LightningRpc, nview *seed.NetworkView) {
-	for {
-		r, err := lrpc.GetNodes()
+// poller regularly polls the backing lnd node and updates the local network
+// view.
+func poller(lnd lnrpc.LightningClient, nview *seed.NetworkView) {
+	ticker := time.NewTicker(time.Second * time.Duration(*pollInterval))
+	for range ticker.C {
 
+		graphReq := &lnrpc.ChannelGraphRequest{}
+		graph, err := lnd.DescribeGraph(
+			context.Background(), graphReq,
+		)
 		if err != nil {
-			log.Errorf("Error trying to get update from lightningd: %v", err)
-		} else {
-			log.Debugf("Got %d nodes from lightningd", len(r.Nodes))
-			for _, n := range r.Nodes {
-				if len(n.Addresses) == 0 {
-					continue
-				}
-				nview.AddNode(n)
-			}
+			log.Errorf("Unable to query for graph: %v", err)
+			continue
 		}
-		time.Sleep(time.Second * time.Duration(*pollInterval))
+
+		log.Debugf("Got %d nodes from lnd", len(graph.Nodes))
+		for _, node := range graph.Nodes {
+			if len(node.Addresses) == 0 {
+				continue
+			}
+			nview.AddNode(node)
+		}
 	}
 }
 
 // Parse flags and configure subsystems according to flags
 func configure() {
 	flag.Parse()
-	expandVariables()
 	if *debug {
 		log.SetLevel(log.DebugLevel)
 	} else {
@@ -65,12 +137,18 @@ func configure() {
 
 // Main entry point for the lightning-seed
 func main() {
+	log.SetOutput(os.Stdout)
+
 	configure()
-	lightningRpc = lightningrpc.NewLightningRpc(*lightningSock)
 
 	nview := seed.NewNetworkView()
 	dnsServer := seed.NewDnsServer(nview, *listenAddr, *rootDomain)
 
-	go poller(lightningRpc, nview)
+	lndNode, err := initLightningClient()
+	if err != nil {
+		log.Fatal("unable to connect to lnd: %v", err)
+	}
+
+	go poller(lndNode, nview)
 	dnsServer.Serve()
 }
