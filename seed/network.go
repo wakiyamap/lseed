c93bea13bb9ba1656f66093c533cd3d55095f45e
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-10-09 12:00:55 -0700

    seed: implement network reachability pruning
    This commit adds a new primary goroutine to the NetworkView struct, the
    reachabilityPruner. The reachabilityPruner’s job is to s to maintain
    the allnodes and reachableNodes map. Each time a new node is added,
    we'll attempt to see if we can actually connect to it. If so, then
    we'll add it to the reachableNodes. Every hour we'll examine the
    reachableNodes map to ensure that all posted nodes are still reachable,
    if not, we'll demote them.
    
    With this change, we ensure that we’ll only return a node to a client
    in response to a query iff, we’re able to reach it reliably as well.
    
    This is only a start. Future follow up changes may add things like
    ensuring the responses return are from distinct ASes, have geographic
    diversity, etc, etc.

--- seed/network.go
+++ seed/network.go
@@ -7,6 +7,7 @@ package seed
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"sync"
 	"time"
 
@@ -15,8 +16,8 @@ import (
 )
 
 const (
-	// Default port for lightning nodes. A and AAAA queries only
-	// return nodes that listen to this port, SRV queries can
+	// defaultPort is the default port for lightning nodes. A and AAAA
+	// queries only return nodes that listen to this port, SRV queries can
 	// actually specify a port, so they return all nodes.
 	defaultPort = 9735
 )
@@ -38,16 +39,36 @@ type Node struct {
 
 // The local view of the network
 type NetworkView struct {
-	nodesMut sync.Mutex
-	nodes    map[string]Node
+	sync.Mutex
+
+	allNodes map[string]Node
+
+	reachableNodes map[string]Node
+
+	freshNodes chan Node
+}
+
+// NewNetworkView creates a new instance of a NetworkView.
+func NewNetworkView() *NetworkView {
+	n := &NetworkView{
+		allNodes:       make(map[string]Node),
+		reachableNodes: make(map[string]Node),
+		freshNodes:     make(chan Node, 100),
+	}
+
+	go n.reachabilityPruner()
+	return n
 }
 
 // Return a random sample matching the NodeType, or just any node if
 // query is set to `0xFF`. Relies on random map-iteration ordering
 // internally.
 func (nv *NetworkView) RandomSample(query NodeType, count int) []Node {
+	nv.Lock()
+	defer nv.Unlock()
+
 	var result []Node
-	for _, n := range nv.nodes {
+	for _, n := range nv.reachableNodes {
 		if n.Type&query != 0 || query == 255 {
 			result = append(result, n)
 		}
@@ -66,8 +87,18 @@ func (nv *NetworkView) AddNode(node *lnrpc.LightningNode) (*Node, error) {
 		LastSeen: time.Now(),
 	}
 
-	for _, addr := range node.Addresses {
-		parsedAddr, err := net.ResolveTCPAddr(addr.Network, addr.Addr)
+	for _, netAddr := range node.Addresses {
+		// If the address doesn't already have a port, we'll assume the
+		// current default port.
+		var addr string
+		_, _, err := net.SplitHostPort(netAddr.Addr)
+		if err != nil {
+			addr = net.JoinHostPort(netAddr.Addr, strconv.Itoa(defaultPort))
+		} else {
+			addr = netAddr.Addr
+		}
+
+		parsedAddr, err := net.ResolveTCPAddr(netAddr.Network, addr)
 		if err != nil {
 			return nil, err
 		}
@@ -89,18 +120,165 @@ func (nv *NetworkView) AddNode(node *lnrpc.LightningNode) (*Node, error) {
 		return nil, fmt.Errorf("node had no addresses")
 	}
 
-	nv.nodesMut.Lock()
-	nv.nodes[n.Id] = *n
-	nv.nodesMut.Unlock()
+	nv.Lock()
+	nv.allNodes[n.Id] = *n
+	nv.Unlock()
 
-	log.Infof("New node %v added n.Id, %v total nodes", n, len(nv.nodes))
+	log.Infof("New node %v added n.Id, %v total nodes", n, len(nv.allNodes))
+
+	go func() {
+		nv.freshNodes <- *n
+	}()
 
 	return n, nil
 }
 
-func NewNetworkView() *NetworkView {
-	return &NetworkView{
-		nodesMut: sync.Mutex{},
-		nodes:    make(map[string]Node),
+func copyNodeMap(a map[string]Node) map[string]Node {
+	n := make(map[string]Node, len(a))
+	for k, v := range a {
+		n[k] = v
+	}
+	return n
+}
+
+// reachabilityPruner is a goroutine whose job it is to maintain the allnodes
+// and reachableNodes map. Each time a new node is added, we'll attempt to see
+// if we can actually connect to it. If so, then we'll add it to the
+// reachableNodes. Every hour we'll examine the reachableNodes map to ensure
+// that all posted nodes are still reachable, if not, we'll demote them.
+func (nv *NetworkView) reachabilityPruner() {
+	// We'll create a new ticker that'll go off every one hour which marks
+	// the start of our reachability pruning.
+	pruneTicker := time.NewTicker(time.Hour)
+
+	// reachableAddrs is a helper function that determines if a node is
+	// reachable or not. In order to determine reachability, we'll attempt
+	// to make a connection on each of the addresses advertised by a node.
+	// The set of reachable addresses for a node are returned.
+	reachableAddrs := func(n Node) []net.TCPAddr {
+		var addrs []net.TCPAddr
+
+		for _, addr := range n.Addresses {
+			// TODO(roasbeef): use brontide to ensure pubkey
+			// identity
+
+			log.Infof("Checking Node(%v) for reachability @ %v",
+				n.Id, addr.String())
+
+			tcpConn, err := net.Dial("tcp", addr.String())
+			if err != nil {
+				log.Infof("Unable to reach %v via %v: %v", n.Id,
+					addr, err)
+				if tcpConn != nil {
+					tcpConn.Close()
+				}
+				continue
+			}
+			tcpConn.Close()
+
+			addrs = append(addrs, addr)
+		}
+
+		return addrs
+	}
+
+	seenNodes := make(map[string]struct{})
+
+	// extractReachableAddrs attempts to move the target node to the
+	// reachableNodes map iff, it has reachable addresses. Only the
+	// addresses marked reachable are added.
+	extractReachableAddrs := func(newNode Node, prune bool) {
+		nv.Lock()
+		if _, ok := seenNodes[newNode.Id]; ok {
+			nv.Unlock()
+			return
+		}
+		nv.Unlock()
+
+		nv.Lock()
+		seenNodes[newNode.Id] = struct{}{}
+		nv.Unlock()
+
+		validAddrs := reachableAddrs(newNode)
+		if len(validAddrs) == 0 {
+			log.Infof("Node(%v) has no reachable addresses, "+
+				"prune=%v", newNode.Id, prune)
+
+			// If prune is no, then if this node has no more
+			// reachable addresses, we'll remove it from out set of
+			// reachable nodes.
+			if prune {
+				nv.Lock()
+				delete(nv.reachableNodes, newNode.Id)
+				nv.Unlock()
+			}
+
+			return
+		}
+
+		newNode.Addresses = validAddrs
+
+		nv.Lock()
+		nv.reachableNodes[newNode.Id] = newNode
+		log.Infof("Node(%v) is reachable number of reachable "+
+			"nodes: %v", newNode.Id, len(nv.reachableNodes))
+		nv.Unlock()
+	}
+
+	for {
+		select {
+		// A new node has just been discovered, if we haven't checked
+		// this node recently, then we'll attempt to see which of its
+		// addresses are reachable.
+		case newNode := <-nv.freshNodes:
+			go func() {
+				extractReachableAddrs(newNode, false)
+			}()
+
+		// The prune timer has ticked, so we'll do two things: try to
+		// move nodes from allNodes to reachableNodes, and also see if
+		// there are any nodes marked reachable which no longer are.
+		case <-pruneTicker.C:
+			log.Infof("Pruning nodes for reachability")
+
+			// First, we'll check to see if any of the nodes that
+			// are within the allNodes, but not reachableNodes map
+			// are now reachable.
+			nv.Lock()
+			allNodes := copyNodeMap(nv.allNodes)
+			nv.Unlock()
+			for _, node := range allNodes {
+				// If it's already marked partially reachable,
+				// then we'll skip over it.
+				//
+				// TODO(roasbeef): query other addrs
+				nv.Lock()
+				if _, ok := nv.reachableNodes[node.Id]; ok {
+					nv.Unlock()
+					continue
+				}
+				nv.Unlock()
+
+				// Otherwise, we'll attempt to filter out it's
+				// set of reachable addresses.
+				extractReachableAddrs(node, false)
+			}
+
+			// Next, we'll possibly prune away any nodes which are
+			// currently in the set of reachable nodes, but which
+			// are no longer reachable.
+			nv.Lock()
+			reachableNodes := copyNodeMap(nv.reachableNodes)
+			nv.Unlock()
+			for _, node := range reachableNodes {
+				extractReachableAddrs(node, true)
+			}
+
+			nv.Lock()
+			log.Infof("Total number of reachable nodes: %v",
+				len(nv.reachableNodes))
+			seenNodes = make(map[string]struct{})
+			nv.Unlock()
+		}
 	}
 }
